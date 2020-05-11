cf7cd38ac093201001156ebfc99cda88227203fb
josh <joshua.sandefer@gmail.com>
2020-05-11 17:52:28 -0500

    did a ton of suff

---
+++ README.md
@@ -0,0 +1,6 @@
+Thoughts: 
+
+1. The user's input will have to go into an array, which is converted to a string when a function is chosen
+2. Once a function is chosen, then any numbers thereafter will have to go into a second array
+3. the two resulting strings are passed through the function and returned as 'ans'
+4. 
--- index.js
+++ index.js
@@ -1,15 +1,48 @@
+//Variables
+let operator = '';
+
 //Basic functions
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-//const divide = (num1, num2) => num1 + num2;
+const add = (arr1, arr2) => arr1 + arr2;
+const subtract = (arr1, arr2) => arr1 - arr2;
+const multiply = (arr1, arr2) => arr1 * arr2;
+//const divide = (arr1, arr2) => arr1 + arr2;
 
 //Button event listener
 const buttons =  document.querySelectorAll('button');
+const display = document.querySelector(['#display-p']);
+let num1 = '';
+let num2 = '';
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-        console.log(e.target);
+        if(e.target.classList.contains('num-button')) {
+            if (operator == '') {
+                num1 += e.target.innerHTML;
+                console.log(num1);         
+                display.innerHTML = num1;
+            } else {
+                num2 += e.target.innerHTML;
+                console.log(num2);         
+                display.innerHTML = num2;
+            }
+        } else if (e.target.classList.contains('function-button')) {
+            display.innerHTML = '0';
+            operator = e.target.id;
+            console.log(operator);
+        }
     });
-})
+});
+
+
 
 //Calculator function
+const calculate = (num1, num2, operator) => {
+    switch(operator){
+        case 'add':
+            add(num1, num2);
+        case 'subtract':
+            subtract(num1, num2);
+        case 'multiply':
+            multiply(num1, num2);    
+    }
+
+}
