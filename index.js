87d7add66aed96b549282846d6e4a0eab9007d17
josh <joshua.sandefer@gmail.com>
2020-05-12 16:15:16 -0500

    working calc

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-//Variables
+//Global variables
 let operator = '';
+let num1 = '';
+let num2 = '';
+let ans = '';
 
 //Basic functions
 const add = (num1, num2) => num1 + num2;
@@ -10,12 +13,16 @@ const divide = (num1, num2) => num1 / num2;
 //Button event listener
 const buttons =  document.querySelectorAll('button');
 const display = document.querySelector(['#display-p']);
-let num1 = '';
-let num2 = '';
+
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         if(e.target.classList.contains('num-button')) {
-            if (operator == '') {
+            if (operator == '' && ans == '') {
+                num1 += e.target.innerHTML;
+                console.log(num1);         
+                display.innerHTML = num1;
+            } else if (operator == '' && !ans == '') {
+                clear();
                 num1 += e.target.innerHTML;
                 console.log(num1);         
                 display.innerHTML = num1;
@@ -28,30 +35,63 @@ buttons.forEach(button => {
             display.innerHTML = '0';
             operator = e.target.id;
             console.log(operator);
+        } else if (e.target.id == 'clear') {
+            clear();
         } else {
+            if (ans == '') {
             console.log(num1, num2, operator);
             calculate(Number(num1), Number(num2), operator);
+            }  else {
+            calculate(Number(ans), Number(num2), operator); 
+            }
         }
     });
 });
 
+//Clear calc
+const clear = () => {
+    num1 = ''; 
+    num2 = ''; 
+    ans = '';
+    operator = '';
+    console.log('clear');
+    display.innerHTML = '0';
+};
+
+const partialClear = () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    console.log('partialCLear');
+}
+
 //Calculator function
 const calculate = (num1, num2, operator) => {
-    switch(operator){
+    if (num2 === 0) {
+        display.innerHTML = 'Yeah right.'
+    } else { 
+        switch(operator) {
         case 'add':
             ans = add(num1, num2);
+            display.innerHTML = ans.toPrecision(8);
             break;
         case 'subtract':
             ans = subtract(num1, num2);
+            display.innerHTML = ans.toPrecision(8);
             break;
         case 'multiply':
             ans = multiply(num1, num2);
+            display.innerHTML = ans.toPrecision(8);
             break;  
         case 'divide':
             ans = divide(num1, num2); 
-            break;  
+            display.innerHTML = ans.toPrecision(8);
+            break; 
+        } 
     }
-    console.log(ans);
-    display.innerHTML = ans;
+    //Need to truncate trailing zeroes from the answer, not sure how to do that yet
+    //display.innerHTML = ans.toPrecision(8);
+    console.log(num2);
+    partialClear()
 
 }
